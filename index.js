8fa385e1848cca219519d20f1053a562283f0337
nurmd7228@gmail.com <nurmd7228@gmail.com>
2024-05-07 06:55:47 +0600

    added server site on cordoctor site , get all data , get a data

---
+++ index.js
@@ -0,0 +1,74 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+
+
+// midilware 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kv4807a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+   //  await client.connect();
+    // Send a ping to confirm a successful connection
+   //  await client.db("admin").command({ ping: 1 });
+
+   const servicesCollection = client.db("carDoctor").collection('servises');
+
+
+
+   app.get('/services', async(req,res) =>{
+      const cursor = servicesCollection.find();
+      const services = await cursor.toArray();
+      res.send(services)
+   })
+
+
+   app.get('/cheakout/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)};
+      const options = {
+
+         // Include only the `title` and `imdb` fields in the returned document
+         projection: { _id: 1, title: 1, price: 1 },
+       };
+      const service = await servicesCollection.findOne(query, options);
+      res.send(service)
+   })
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   //  await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res) =>{
+   res.send('Hellodadsasd World');
+});
+
+
+app.listen(PORT, () =>{
+   console.log(`server is running on ${PORT}`);
+})
